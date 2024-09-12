c9d1aa89598cb1dfff522416078410f0cc2dabd5
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-13 03:41:22 +0500

    About-Us

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -29,27 +29,27 @@ function AboutUs() {
               About Us
             </Typography>
 
-            <Typography variant="body1">
+            <Typography variant="body1" paragraph>
               Welcome to Lelo, where we are committed to providing the best
               products and services in the industry. Our journey began in one
               Year, and since then, we have dedicated ourselves to meeting the
               needs of our customers with the highest standards of quality and
               customer service.
             </Typography>
-            <Typography variant="body2">
+            <Typography variant="body2" paragraph>
               Our team consists of passionate professionals who are experts in
               their respective fields. We believe in innovation, integrity, and
               the importance of building strong relationships with our clients.
               These core values guide us in every project we undertake.
             </Typography>
-            <Typography variant="body3">
+            <Typography variant="body3" paragraph>
               At Lelo, we strive to create a positive impact not only for our
               customers but also for our community and the environment. We are
               constantly exploring new ways to improve our processes and
               products, ensuring that we stay ahead of the curve in an
               ever-changing marketplace.
             </Typography>
-            <Typography variant="body4">
+            <Typography variant="body4" paragraph>
               Thank you for choosing Lelo. We look forward to continuing to
               serve you with excellence and dedication.
             </Typography>
