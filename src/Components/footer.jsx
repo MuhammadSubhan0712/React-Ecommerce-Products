98e708b5bb07808e5f7b1e168a3f817cf630b59f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-13 02:16:59 +0500

    Single-Page-App

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Typography from '@mui/material/Typography';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Typography from "@mui/material/Typography";
 
 const Footer = () => {
+  return (
+    <>
+      <footer className="bg-dark text-white text-center py-3">
+        <Typography variant="body1">
+          &copy; 2024 E-Commerce. All rights reserved.
+        </Typography>
+      </footer>
+    </>
+  );
+};
 
-    return(
-   <>
-   <footer className="bg-dark text-white text-center py-3">
-   <Typography variant="body1">
-     &copy; 2024 E-Commerce. All rights reserved.
-   </Typography>
- </footer>
- </>
-    )
-}
-
-export default Footer
-   
-   
+export default Footer;
