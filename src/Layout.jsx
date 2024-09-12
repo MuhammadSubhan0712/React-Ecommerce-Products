f18facf24696800c9cc5c99dd99787e49826e45f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-12 21:46:13 +0500

    Ecommerce Products App

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,14 @@
+// import React from "react"
+import { Outlet } from "react-router-dom"
+import Navbar from "./Components/Navbar"
 
 
 const Layout = () => {
 
     return(
         <>
+        <Navbar/>
+        <Outlet/>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,55 @@
-import '@fontsource/roboto/400.css';
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "@fontsource/roboto/400.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Layout from "./Layout.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./Pages/Home.jsx";
+import Contact from "./Pages/Contact.jsx";
+import About from "./Pages/About.jsx";
+import Product from "./Pages/Products.jsx";
+import SingleProduct from "./Pages/SingleProduct.jsx";
+import { Typography } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
-  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "products",
+        element: <Product />,
+      },
+      {
+        path: "singleproduct",
+        element: <SingleProduct />,
+      },
+      {
+        path: "*",
+        element: (
+          <Typography className="d-flex justify-content-center">
+            Not Found
+          </Typography>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}>
     <App />
-
-)
+  </RouterProvider>
+);
