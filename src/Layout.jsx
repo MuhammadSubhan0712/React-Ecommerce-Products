f645db1a884336394e37f88518c10afa8c84d51f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-13 02:17:08 +0500

    Single-Page-App

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,16 @@
 // import React from "react"
-import { Outlet } from "react-router-dom"
-import ResponsiveAppBar from "./Components/Appbar"
-import Footer from "./Components/footer"
+import { Outlet } from "react-router-dom";
+import ResponsiveAppBar from "./Components/Appbar";
+import Footer from "./Components/footer";
 
 const Layout = () => {
-    return(
-        <>
-        <ResponsiveAppBar/>
-        <Outlet/>
-        <Footer/>
-       </>
-    )
-}
+  return (
+    <>
+      <ResponsiveAppBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
